0de76022b08798d21c95d3bb735f4b7ba448c8c9
jestro <jestro.roothans@student.howest.be>
2023-12-24 16:07:33 +0100

    Fixed, renamed and moved my ASCII encoder.

--- CryptoHack/Intro/ASCII/ASCII_enco.py
+++
@@ -1,7 +0,0 @@
-def ascii_decode(plaintext):
-    cipher = ""
-
-    for plaintext_char in plaintext:
-        cipher += ord(plaintext_char)
-
-    return cipher
---
+++ Encoding/ASCII/ASCII_encode.py
@@ -0,0 +1,7 @@
+def ascii_encode(plaintext):
+    ascii_list = []
+
+    for plaintext_char in plaintext:
+        ascii_list.append(ord(plaintext_char))
+
+    return ascii_list
