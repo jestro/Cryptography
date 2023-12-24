b636347e8921fd1eaa3a9e629462eb198d7359ff
jestro <jestro.roothans@student.howest.be>
2023-12-24 16:08:18 +0100

    Renamed a variable and changed name of the file.

--- Encoding/ASCII/ASCII_decode.py
+++ Encoding/ASCII/ASCII_decode.py
@@ -1,7 +1,7 @@
-def ascii_decode(cipher):
+def ascii_decode(ascii_list):
     plain_text = ""
 
-    for ascii_char in cipher:
+    for ascii_char in ascii_list:
         plain_text += chr(ascii_char)
 
     return plain_text
