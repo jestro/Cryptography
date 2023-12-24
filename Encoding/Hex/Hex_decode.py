887722b859bdcdd98e41867d86f855338b6c5934
jestro <jestro.roothans@student.howest.be>
2023-12-24 17:55:32 +0100

    Removed the remove_0x function as the expected input should be correct.

--- Encoding/Hex/Hex_decode.py
+++ Encoding/Hex/Hex_decode.py
@@ -1,24 +1,20 @@
-from Encoding.HelperFunctions.Hex import remove_0x
+from Encoding.ASCII.ASCII_encode import ascii_encode
 
 
 # "637279" -> b'cry' (bytes)
 def hex_decode_plaintext(hexstring):
-    remove_0x(hexstring)
-
     return bytes.fromhex(hexstring)
 
 
 # "637279" -> 6517369
-def hex_decode_number(hexcode):
-    remove_0x(hexcode)
-
+def hex_decode_number(hexstring):
     hex_values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
     hex_eq = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    hexcode = hexcode.upper()
+    hexstring = hexstring.upper()
 
     number = 0
 
-    for index in range(0, len(hexcode)):
-        last_index = len(hexcode) - 1
-        number += hex_eq[hex_values.index(hexcode[index])] * pow(16, last_index - index)
+    for index in range(0, len(hexstring)):
+        last_index = len(hexstring) - 1
+        number += hex_eq[hex_values.index(hexstring[index])] * pow(16, last_index - index)
     return number
