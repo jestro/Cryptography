a234b0b6b7c8c7ca5828a61fd2775045dd84a2f1
jestro <jestro.roothans@student.howest.be>
2023-12-24 22:00:28 +0100

    Made them easier to read and understand.

--- Encoding/Binary/Binary_decode.py
+++ Encoding/Binary/Binary_decode.py
@@ -2,40 +2,40 @@
 # format -> '0b1100011' or ['0b1100011', ...]
 
 # binary -> decimal
-def binary_decode_number(binary_list):
-    if isinstance(binary_list, str):
-        return int(binary_list, 2)
+def bin_to_int(bin_list):
+    if isinstance(bin_list, str):
+        return int(bin_list, 2)
 
-    ascii_list = []
+    int_list = []
 
-    for binary_char in binary_list:
-        ascii_list.append(int(binary_char, 2))
+    for binary_char in bin_list:
+        int_list.append(int(binary_char, 2))
 
-    return ascii_list
+    return int_list
 
 
 # Only possible if the binary items are Decimal
 # binary -> decimal -> plaintext
-def binary_decode_plaintext(binary_list):
-    if isinstance(binary_list, str):
-        return chr(int(binary_list, 2))
+def bin_to_chr(bin_list):
+    if isinstance(bin_list, str):
+        return chr(int(bin_list, 2))
 
-    plain_text = ""
+    plaintext = ""
 
-    for binary_char in binary_list:
-        plain_text += chr(int(binary_char, 2))
+    for binary_char in bin_list:
+        plaintext += chr(int(binary_char, 2))
 
-    return plain_text
+    return plaintext
 
 
 # binary -> decimal -> hex
-def binary_decode_hex(binary_list):
-    if isinstance(binary_list, str):
-        return hex(int(binary_list, 2))
+def bin_to_hex(bin_list):
+    if isinstance(bin_list, str):
+        return hex(int(bin_list, 2))
 
     hex_list = []
 
-    for binary_char in binary_list:
+    for binary_char in bin_list:
         hex_list.append(hex(int(binary_char, 2)))
 
     return hex_list
--- Encoding/Hex/Hex_decode.py
+++ Encoding/Hex/Hex_decode.py
@@ -2,40 +2,40 @@
 # format -> '0x63' or ['0x63', ...]
 
 # hex -> decimal
-def hex_decode_decimal(hex_list):
+def hex_to_int(hex_list):
     if isinstance(hex_list, str):
         return int(hex_list, 16)
 
-    ascii_list = []
+    int_list = []
 
     for hex_char in hex_list:
-        ascii_list.append(int(hex_char, 16))
+        int_list.append(int(hex_char, 16))
 
-    return ascii_list
+    return int_list
 
 
 # Only possible if the hex items' decimal values are ASCII
 # hex -> decimal -> plaintext
-def hex_decode_plaintext(hex_list):
+def hex_to_chr(hex_list):
     if isinstance(hex_list, str):
         return chr(int(hex_list, 16))
 
-    plain_text = ""
+    plaintext = ""
 
     for hex_char in hex_list:
-        plain_text += chr(int(hex_char, 16))
+        plaintext += chr(int(hex_char, 16))
 
-    return plain_text
+    return plaintext
 
 
 # hex -> decimal -> binary
-def hex_decode_binary(hex_list):
+def hex_to_bin(hex_list):
     if isinstance(hex_list, str):
         return bin(int(hex_list, 16))
 
-    binary_list = []
+    bin_list = []
 
     for hex_char in hex_list:
-        binary_list.append(bin(int(hex_char, 16)))
+        bin_list.append(bin(int(hex_char, 16)))
 
-    return binary_list
+    return bin_list
