4b07bd7cd4a39810ceb355503eca02d6508789e3
jestro <jestro.roothans@student.howest.be>
2023-12-24 16:50:49 +0100

    Added a separate file for helper functions.

---
+++ HelperFunctions/string_manip.py
@@ -0,0 +1,12 @@
+def substring(string, start, end):
+    return string[start:end]
+
+
+# Also works on arrays
+def split(string, split_amount):
+    splitted_string = []
+
+    for index in range(0, len(string), split_amount):
+        splitted_string.append(substring(string, index, index + split_amount))
+
+    return splitted_string
