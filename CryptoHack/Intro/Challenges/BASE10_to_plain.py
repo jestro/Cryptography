fbeb0ef2d84a08a776eb77e71ed662c4baa5d748
jestro <jestro.roothans@student.howest.be>
2023-12-25 01:17:42 +0100

    Changed solutions to work again.

--- CryptoHack/Intro/Challenges/BASE10_to_plain.py
+++ CryptoHack/Intro/Challenges/BASE10_to_plain.py
@@ -1,8 +1,11 @@
-from Encoding.Hex.Hex_decode import hex_decode_plaintext_bytes
+import codecs
+
+from Encoding.Hex.Hex_decode import hex_to_chr
 
 number = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
 hexstring = hex(number)[2:]
-plaintext = hex_decode_plaintext_bytes(hexstring)
+plaintext = hex_to_chr(hexstring)
+
 
 print(number)
 print(hexstring)
--- CryptoHack/Intro/Challenges/Hex_to_BASE64.py
+++ CryptoHack/Intro/Challenges/Hex_to_BASE64.py
@@ -1,10 +1,9 @@
 import base64
-from Encoding.Hex.Hex_decode import hex_decode_plaintext_bytes
 
 hexstring = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
-string = hex_decode_plaintext_bytes(hexstring)
-base64 = base64.b64encode(string)
+hexbytes = bytes.fromhex(hexstring)
+plaintext = base64.b64encode(hexbytes)
 
 print(hexstring)
-print(string)
-print(base64)
+print(hexbytes)
+print(plaintext)
