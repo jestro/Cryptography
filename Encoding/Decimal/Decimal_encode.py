ff6e3d72be1763674e4f24026127c88db3ac70e4
jestro <jestro.roothans@student.howest.be>
2023-12-28 00:32:35 +0100

    Changed the chr_to_int to be to an actual decimal and separated it from the ASCII list.

--- Encoding/Decimal/Decimal_encode.py
+++ Encoding/Decimal/Decimal_encode.py
@@ -1,14 +1,24 @@
+from Encoding.FormatFunctions.Hex import merge_hex_array
+from Encoding.Hex.Hex_decode import hex_to_int
+from Encoding.Hex.Hex_encode import chr_to_hex
+
 # list or singular item
 # format -> c or cry
 
-# plaintext -> decimal
-def chr_to_int(plaintext):
+
+# plaintext -> ASCII list
+def chr_to_ascii_array(plaintext):
     if len(plaintext) == 1:
         return ord(plaintext)
 
-    int_list = []
+    ascii_list = []
 
     for char in plaintext:
-        int_list.append(ord(char))
+        ascii_list.append(ord(char))
 
-    return int_list
+    return ascii_list
+
+
+# plaintext -> hex -> decimal
+def chr_to_int(plaintext):
+    return hex_to_int(merge_hex_array(chr_to_hex(plaintext)))
