c24ff825493cbd5f66348db0aea02fd753bdab96
jestro <jestro.roothans@student.howest.be>
2023-12-28 12:11:22 +0100

    Added hex, binary and int support. Adapted all the format functions.

--- Encryption/XOR/Helper.py
+++ Encryption/XOR/Helper.py
@@ -1,24 +1,39 @@
-from Encoding.Binary.Binary_encode import chr_to_bin
-from Encoding.Decimal.Decimal_decode import int_to_bin
-from Encoding.FormatFunctions.Binary import format_bin_8bit
-from Encoding.FormatFunctions.Check import is_codetype
-from HelperFunctions.list_manip import is_list
+from Encoding.Numeral.Binary.Binary_encode import chr_to_bin
+from Encoding.Numeral.Decimal.Decimal_decode import int_to_bin
+from Encoding.HelperFunctionsEnc.Binary import format_bin
+from Encoding.HelperFunctionsEnc.Check import is_codetype
+from Encoding.Numeral.Hex.Hex_decode import hex_to_bin
+from HelperFunctionsGlobal.list_manip import is_list
 
 
-def key_init(key):
-    key_list = []
+def data_to_bin7b(data):
+    bin_list = []
 
-    if is_codetype(key) == "str":
-        if len(key) > 1:
-            key_list = chr_to_bin(key)
+    if is_codetype(data) == "str":
+        if len(data) > 1:
+            bin_list = chr_to_bin(data)
         else:
-            key_list = [chr_to_bin(key)]
+            bin_list = [chr_to_bin(data)]
 
-    elif isinstance(key, int):
-        if is_list(key):
-            for binary in int_to_bin(key):
-                key_list.append(format_bin_8bit(binary))
+    elif is_codetype(data) == "int":
+        if is_list(data):
+            for binary in int_to_bin(data):
+                bin_list.append(format_bin(binary, 7))
         else:
-            key_list = [format_bin_8bit(int_to_bin(key))]
+            bin_list = [format_bin(int_to_bin(data), 7)]
 
-    return key_list
+    elif is_codetype(data) == "binary":
+        if is_list(data):
+            for binary in data:
+                bin_list.append(format_bin(binary, 7))
+        else:
+            bin_list = [format_bin(data, 7)]
+
+    elif is_codetype(data) == "hex":
+        if is_list(data):
+            for binary in hex_to_bin(data):
+                bin_list.append(format_bin(binary, 7))
+        else:
+            bin_list = [format_bin(hex_to_bin(data), 7)]
+
+    return bin_list
