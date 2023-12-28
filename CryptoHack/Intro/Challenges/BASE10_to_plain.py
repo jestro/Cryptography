e7a95bef8012ee1a89e348efd53dfc120ac0d4c5
jestro <jestro.roothans@student.howest.be>
2023-12-28 11:47:24 +0100

    Replaced with a function.

--- CryptoHack/Intro/Challenges/BASE10_to_plain.py
+++ CryptoHack/Intro/Challenges/BASE10_to_plain.py
@@ -1,12 +1,5 @@
-import codecs
-
-from Encoding.Hex.Hex_decode import hex_to_chr
+from Encoding.Numeral.Decimal.Decimal_decode import int_to_chr
 
 number = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
-hexstring = hex(number)[2:]
-plaintext = hex_to_chr(hexstring)
 
-
-print(number)
-print(hexstring)
-print(plaintext)
+print(int_to_chr(number))
