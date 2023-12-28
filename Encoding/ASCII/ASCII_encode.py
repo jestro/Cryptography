6951cbb202f5c8dd9c878e5e361332b0161d035f
jestro <jestro.roothans@student.howest.be>
2023-12-28 11:03:08 +0100

    Made hex and bin to ascii a bit more flexible. Changed comments.

--- Encoding/ASCII/ASCII_encode.py
+++ Encoding/ASCII/ASCII_encode.py
@@ -3,8 +3,8 @@ from Encoding.FormatFunctions.Hex import remove_0x
 from HelperFunctions.string_manip import split
 
 
-# plaintext -> ASCII list
-def chr_to_ascii_array(plaintext):
+# plaintext -> ASCII (if singular) / ASCII list
+def chr_to_ascii_list(plaintext):
     if len(plaintext) == 1:
         return ord(plaintext)
 
@@ -16,11 +16,14 @@ def chr_to_ascii_array(plaintext):
     return ascii_list
 
 
-# binary -> ASCII list
-def bin_to_ascii_array(bin_list):
+# binary list / binary string -> ASCII (if singular) / ASCII list
+def bin_to_ascii_list(bin_list):
     if isinstance(bin_list, str):
         bin_list = remove_0b(bin_list)
         bin_list = split(bin_list, 7)
+
+    if len(bin_list) == 1:
+        return int(bin_list[0], 2)
 
     ascii_list = []
 
@@ -30,11 +33,14 @@ def bin_to_ascii_array(bin_list):
     return ascii_list
 
 
-# hex -> ASCII list
-def hex_to_ascii_array(hex_list):
+# hex list / hex string -> ASCII (if singular) / ASCII list
+def hex_to_ascii_list(hex_list):
     if isinstance(hex_list, str):
         hex_list = remove_0x(hex_list)
         hex_list = split(hex_list, 2)
+
+    if len(hex_list) == 1:
+        return int(hex_list[0], 16)
 
     ascii_list = []
 
